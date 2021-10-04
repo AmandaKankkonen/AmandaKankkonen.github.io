b24fa7c725b1bc3bc70e06f23c4b2bf90bd25598
AmandaKankkonen <amandakankkonen@gmail.com>
2021-10-04 15:15:11 +0300

    04/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // App.tsx
-import React from 'react'
+import React, { useState } from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
+import clickCount from './components/clickCount';
 
 const App: React.FC = () => {
   return (
     <div>
       <MyFirstComponent />
       <SomeExampleComponent />
+      <ClickCounter />
     </div>
   )
 }
