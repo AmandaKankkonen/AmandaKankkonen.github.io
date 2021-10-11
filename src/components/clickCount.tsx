7c63f6da6569ffd4834d795ca44f7d320d5dfc2a
AmandaKankkonen <amandakankkonen@gmail.com>
2021-10-11 13:05:43 +0300

    11/10

--- src/components/clickCount.tsx
+++ src/components/clickCount.tsx
@@ -5,15 +5,15 @@ type ClickCountProps = {
 }
 
 const ClickCount: React.FC<ClickCountProps> = () => {
-  const [count, setCount] = useState(0);
-  <button onClick={() => setCount(count + 1)}>+1</button>  
+  const [count, setCount] = useState(0); 
   useEffect(() => {
     console.log(`Clicked the button ${count} times`);
 }, [count]);
 
   return (
     <div>
-      ClickCount
+    <button onClick={() => setCount(count + 1)}>+1</button> 
+    <p>Button clicked {count} times</p> 
     </div>
   )
 }
