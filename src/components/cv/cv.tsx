f28194d2459e5b5948f176c2a6a533913b15dfa6
AmandaKankkonen <amandakankkonen@gmail.com>
2022-01-27 11:36:01 +0200

    Final updates

--- src/components/cv/cv.tsx
+++ src/components/cv/cv.tsx
@@ -8,7 +8,7 @@ type CVProps = {
 const CV: React.FC<CVProps> = () => {
   return (
     <div className={styles.container}>
-      <h1>Koulutus / Schooling</h1>
+      <h2>Koulutus / Schooling</h2>
       <ul><li>Varia ammattikoulu -ICT Ohjelmointi- 01/02/2021-present/nykyään</li><br/>
       <li>Suomi B1.2-B2.1 Intensiivikurssi -Omnia- 31/08/2020-28/11/2020</li><br/>
       <li>YKI-Valmentava -Helsingin Työväenopisto- 19/01/2020-09/03/2020</li><br/>
@@ -17,7 +17,7 @@ const CV: React.FC<CVProps> = () => {
       <li>Suomen kieli A1-B1 -Arffman- 12/11/2018-13/05/2019</li><br/>
      <li>Lukio/High School -Dunedin HS- 2008 </li></ul>
 
-<h1>Viimeaikainen Työhistoria / Recent Employment History</h1>
+<h2>Viimeaikainen Työhistoria / Recent Employment History</h2>
 <ul>
 <li>Artist/Taitelija -Tanssija/Dancer- 31/08/2011-present/nykyään</li><br/>
 <li>Nanny/Au pair -Sara Ludena- 04/01/2014-06/06/2017 </li></ul><br/>
