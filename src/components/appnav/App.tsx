585c18959be1a14ecc191542bebe46e4c546c9c8
AmandaKankkonen <amandakankkonen@gmail.com>
2022-02-15 14:02:02 +0200

    Icons/Experience updated

--- src/components/appnav/App.tsx
+++ src/components/appnav/App.tsx
@@ -9,6 +9,8 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { GrGithub } from 'react-icons/gr';
 
 const App: React.FC = () => {
   return (
@@ -19,7 +21,8 @@ const App: React.FC = () => {
        <div className={styles.navbar}>
          <Link to='portfolio'>Portfolio</Link>
          <Link to='cv'>CV</Link>
-         <a href='https://www.linkedin.com/in/amanda-kankkonen-038853212/' target="_blank" rel="noreferrer noopener">LinkedIn</a>
+         <a href='https://github.com/AmandaKankkonen?tab=repositories' target="_blank" rel="noreferrer noopener"><GrGithub color="lightblue" size="23" /></a>
+         <a href='https://www.linkedin.com/in/amanda-kankkonen-038853212/' target="_blank" rel="noreferrer noopener"><AiOutlineLinkedin color="lightblue" size="23" /></a>
        </div>
        <AboutMe/>
        <Switch>
