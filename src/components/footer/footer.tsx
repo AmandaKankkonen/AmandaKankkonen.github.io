770e495e03958f75aa42b7506d578abec813ac52
unknown <amandakankkonen@gmail.com>
2022-03-03 11:33:00 +0200

    Email icon added

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import styles from '../cv/cv.module.scss'
 import styles from './footer.module.scss';
+import photo from './email.png'
 
 type FooterProps = {
 
@@ -9,6 +10,9 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className={styles.container}>
+      <div className={styles.icon}>
+        <img src={photo} alt='emailIcon'/> 
+      </div>
       <a href="mailto: amandakankkonen@gmail.com">
         amandakankkonen@gmail.com
       </a>
