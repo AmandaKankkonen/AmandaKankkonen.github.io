729b2c5ace4d2e2fea7761449b9bccda930d3f61
unknown <amandakankkonen@gmail.com>
2022-08-02 16:29:29 +0300

    Updates

--- src/components/aboutme/aboutMe.tsx
+++ src/components/aboutme/aboutMe.tsx
@@ -18,15 +18,15 @@ const AboutMe: React.FC<AboutMeProps> = () => {
         <p>Hello, my name is</p>
         <h1>Amanda Kankkonen</h1>
         <p>Originally from the US, I am a 2nd year programming student currently 
-           studying at Varia in Vantaa, Finland. During my programming studies at
-           Varia I have been effeciently taught how to create websites that are not 
-           only attractive, but functional on both the front and back end. I've learned 
-           to make fun, engaging games using C# and Unity - both independently and in 
-           collaboration with my colleagues utilizing Agile/Scrum methods. Most of my 
-           framework experience is with React, and I prefer VScode. I love to learn new 
-           things and can't wait to see what the future offers. Native English, Fluent 
-           Finnish.
-          <br></br>Currently learning: Lua
+           studying at Varia in Vantaa, Finland. During my programming studies at Varia
+           I have been efficiently taught how to create unique, engaging games using C# 
+           and Unity - both independently and in collaboration with my colleagues utilizing
+           Agile/Scrum methods. I've learned to make websites that are not only attractive,
+           but functional on both the front and back end. Most of my framework experience
+           is with React, and I prefer VScode. I love to learn new things and can't wait to
+           see what the future offers. I am nearing the end of my program (Class of 2023) and
+           am available for full-time work. Native English, Fluent Finnish. 
+          <br></br>Currently learning: Lua, Kotlin, iOS/Android mobile games
         </p>
           <h2>My language experience includes:</h2>
         <p>Java, Javascript, Typescript, React, C#, HTML & CSS/SASS, SQL/NoSQL</p>
